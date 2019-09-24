eb2f8efb7bf8f6eba9ce1205f9540a3c60182bbf
Chelsi Christmas <chelsichristmas@Chelsis-MacBook-Pro.local>
2019-09-24 15:52:19 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Chelsi Christmas on 9/24/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Chelsi")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
